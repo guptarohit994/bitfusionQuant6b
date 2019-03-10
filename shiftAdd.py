5edea5549c46bb64c54782cb9b27b0ea2584d04d
Krishna14 <krishna1994sree@gmail.com>
2019-03-10 16:09:16 -0700

    Restarted commit

--- shiftAdd.py
+++ shiftAdd.py
@@ -5,11 +5,12 @@
 
 class shiftAdd():
     """Class to implement a shiftAdd"""
-    def __init__(self, shiftAmts, inputs):
+    def __init__(self, shiftAmts, inputs, level):
         """Constructor to initialize the shiftAmts and inputs"""
         print("shiftAdd.py<-__init__: inputs="+"self:"+str(self)+",")
         self.shiftAmts = shiftAmts
         self.inputs = inputs
+        self.level = level
 
     def computeSum(self, shiftAmts, inputs):
         """Returns the sum based on the inputs"""
@@ -24,10 +25,15 @@ class shiftAdd():
         """Returns the final sum and latency for performing this operation"""
         print("shiftAdd.py<-printFinalSum: inputs="+"self:"+str(self)+",")
         FinalSum = self.computeSum(self.shiftAmts, self.inputs)
-        return [FinalSum, 2.0]
+        if self.level == 0:
+            return [FinalSum, 2.0]
+        else:
+            return [FinalSum, 4.0]
 
 
 
 if __name__ == '__main__':
-    SA0 = shiftAdd([0, 2, 2, 4], [2, 2, 4, 2])
+    SA0 = shiftAdd([0, 2, 2, 4], [2, 2, 4, 2], 0)
     print(SA0.printFinalSum_Latency())
+    SA1 = shiftAdd([0, 4, 4, 8], [2, 2, 4, 2], 1)
+    print(SA1.printFinalSum_Latency())
